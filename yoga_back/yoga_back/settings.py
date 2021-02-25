6b3e0c8da515b20a7d86217ea3d118299eeacea5
root <root@vps-36418.vps-default-host.net>
2021-02-25 12:33:24 +0200

    config udpates

--- yoga_back/yoga_back/settings.py
+++ yoga_back/yoga_back/settings.py
@@ -93,8 +93,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'yoga',
-        'USER': 'postgres',
-        'PASSWORD': '1111',
+        'USER': 'yoga_dev',
+        'PASSWORD': 'yoga_dev',
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -155,9 +155,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/doc') ,
     ]
 STATIC_URL = '/static/'
-
+STATIC_ROOT = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 REST_FRAMEWORK = {
